f1c283dced62f5f478a370e4a4fd85f83f457949
Robert Joleman <bob.joleman@gmail.com>
2012-02-22 00:55:42 -0500

    updated itemrec 2

--- itemrec.php
+++ itemrec.php
@@ -7,6 +7,31 @@ function array_count_values_of($value, $array) {
     return $counts[$value];
 }
 
+function check_pairs($first, $second, $array1, $array2) {
+	if (!empty($array1)){
+		$keys = array_keys($array1, $first);
+		$keys2 = array_keys($array1, $second);
+	}
+	else {return False;}
+	$yes = 0;
+	if (!empty($keys)) {
+		foreach ($keys as $key) {
+			if ($array2[$key]==$second) {
+				$yes = 1;
+				}
+			}
+		}
+	if (!empty($keys2)) {
+		foreach ($keys2 as $key2) {
+			if ($array2[$key2]==$first) {
+				$yes = 1;
+				}
+			}
+		}
+	if ($yes==0) {return False;}
+	else {return True;}
+}
+
 //redefine as session var later
 $user = "Tony";
 $user_dislikes = array();
@@ -46,6 +71,19 @@ if (!empty($_POST[fpair])) {
 $user_dislikes1 = mysql_query("SELECT second_item FROM $user WHERE first_item = chosen_item");
 $user_dislikes2 = mysql_query("SELECT first_item FROM $user WHERE second_item = chosen_item");
 
+$user_first = mysql_query("SELECT first_item FROM $user");
+$user_second = mysql_query("SELECT second_item FROM $user");
+
+$user_first_row = array();
+$user_second_row = array();
+
+while ($user_first_row1 = mysql_fetch_array($user_first)) {
+	array_push($user_first_row, $user_first_row1['first_item']);
+}
+while ($user_second_row1 = mysql_fetch_array($user_second)) {
+	array_push($user_second_row, $user_second_row1['second_item']);
+}
+
 while ($row = mysql_fetch_array($user_dislikes1)) {
 	array_push($user_dislikes, $row['second_item']);
 }
@@ -69,10 +107,10 @@ $query = mysql_query("SELECT first_item, second_item FROM master");
 			$first = $rowww['first_item'];
 			$queryy = mysql_query("SELECT first_item, second_item, total_pairings, total_first_items FROM master WHERE first_item = '$first'");
 			while ($rowwww = mysql_fetch_array($queryy)) {
-				if (($rowwww['total_pairings']==0) && ((array_count_values_of($rowwww['second_item'], $user_dislikes))<$dislike_tolerance)) {
+				if (($rowwww['total_pairings']==0) && ((array_count_values_of($rowwww['second_item'], $user_dislikes))<$dislike_tolerance)  && (check_pairs($first, $rowwww['second_item'], $user_first_row, $user_second_row)==0)) {
 					$choice = $rowwww['second_item'];
 					}
-				elseif ((($rowwww['total_first_items']/$rowwww['total_pairings'])>$nchoice) && ((array_count_values_of($rowwww['second_item'], $user_dislikes))<$dislike_tolerance)) {
+				elseif ((($rowwww['total_first_items']/$rowwww['total_pairings'])>$nchoice) && ((array_count_values_of($rowwww['second_item'], $user_dislikes))<$dislike_tolerance) && (check_pairs($first, $rowwww['second_item'], $user_first_row, $user_second_row)==0)) {
 					$nchoice = ($rowwww['total_first_items']/$rowwww['total_pairings']);
 					$choice = $rowwww['second_item'];
 					}
@@ -80,10 +118,10 @@ $query = mysql_query("SELECT first_item, second_item FROM master");
 			
 			$queryyy = mysql_query("SELECT first_item, second_item, total_pairings, total_first_items FROM master WHERE second_item = '$first'");
 			while ($rowwwww = mysql_fetch_array($queryyy)) {
-				if (($rowwwww['total_pairings']==0) && ((array_count_values_of($rowwwww['first_item'], $user_dislikes))<$dislike_tolerance)) {
+				if (($rowwwww['total_pairings']==0) && ((array_count_values_of($rowwwww['first_item'], $user_dislikes))<$dislike_tolerance) && (check_pairs($first, $rowwwww['first_item'], $user_first_row, $user_second_row)==0)) {
 					$choice = $rowwwww['first_item'];
 					}
-				elseif (((1-($rowwwww['total_first_items']/$rowwwww['total_pairings']))>$nchoice) && ((array_count_values_of($rowwwww['first_item'], $user_dislikes))<$dislike_tolerance)) {
+				elseif (((1-($rowwwww['total_first_items']/$rowwwww['total_pairings']))>$nchoice) && ((array_count_values_of($rowwwww['first_item'], $user_dislikes))<$dislike_tolerance) && (check_pairs($first, $rowwwww['first_item'], $user_first_row, $user_second_row)==0)) {
 					$nchoice = ($rowwwww['total_first_items']/$rowwwww['total_pairings']);
 					$choice = $rowwwww['first_item'];
 					}
